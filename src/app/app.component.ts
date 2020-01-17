3fa88bdbc491bfc26a8ca7fb1e41c7d8a36854d7
kk <k.klapczynski@gmail.com>
2020-01-17 10:30:54 +0100

    210: Own validation function.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
   hobbiesFormArray: FormArray;
+  forbiddenNames = ['Chris', 'Anna'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userdata': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.isForbiddenName.bind(this)]),  // isForbiddenName function is called as validator somewhere by Angular, that is why here we bind to our component where forbiddenNames are defined
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('male'),
@@ -34,5 +35,14 @@ export class AppComponent implements OnInit {
 
   get hobbiesControls() {
     return this.hobbiesFormArray.controls;
+  }
+
+  // our own validation function
+  isForbiddenName(formControl: FormControl) : { [s: string] : boolean } {
+    if( this.forbiddenNames.indexOf(formControl.value) !== -1) {
+      return {'isForbiddenName' : true};
+    } else {
+      return null;
+    }
   }
 }
