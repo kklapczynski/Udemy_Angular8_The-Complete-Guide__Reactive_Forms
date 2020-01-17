af5e91ed97e41a1b9175b2b9ad0dfc0e2dad21a1
kk <k.klapczynski@gmail.com>
2020-01-17 11:03:00 +0100

    212: Asynchronous validation function.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userdata': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.isForbiddenName.bind(this)]),  // isForbiddenName function is called as validator somewhere by Angular, that is why here we bind to our component where forbiddenNames are defined
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.isForbiddenEmail), // asynchronous validators are a third argument: single or array of them
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -44,5 +45,19 @@ export class AppComponent implements OnInit {
     } else {
       return null;
     }
+  }
+
+  // asynchronous validation function: remember to pass it as a 3rd argument to FormControl constructor
+  isForbiddenEmail(formControl: FormControl) : Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(formControl.value === "test@test") {
+          resolve({'isEmailForbidden' : true});
+        } else {
+          resolve(null);
+        }
+      }, 2000);
+    });
+    return promise;
   }
 }
